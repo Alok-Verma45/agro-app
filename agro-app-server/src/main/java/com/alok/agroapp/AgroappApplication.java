5be2e32bce6ff605e7540799c2b3328e547edcb9
Alok-Verma45 <alokkumar09921@gmail.com>
2026-04-09 23:04:44 +0530

    Initial project structure with backend setup

---
+++ agro-app-server/src/main/java/com/alok/agroapp/AgroappApplication.java
@@ -0,0 +1,13 @@
+package com.alok.agroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroappApplication.class, args);
+	}
+
+}
