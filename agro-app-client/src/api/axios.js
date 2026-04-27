5ca67ea073829d61a9334aec4d8ef76cbb5d2654
Alok-Verma45 <alokkumar09921@gmail.com>
2026-04-28 04:40:06 +0530

    Temporary hardcoded backend URL for production test

--- agro-app-client/src/api/axios.js
+++ agro-app-client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + "/api"
+  baseURL: "https://agro-backend-p3nq.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
